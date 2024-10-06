32860a2df12e305600b6c36e305032da0357c05e
JhonRRA <jhon.rojas.a@uni.pe>
2024-10-06 16:15:57 -0500

    Cubo

--- main.py
+++ main.py
@@ -19,20 +19,25 @@ while running:
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
+    
+    if player_pos.y < 880:
 
-    pygame.draw.rect(screen, "yellow", (player_pos[0],player_pos[1],40,40))
+        pygame.draw.rect(screen, "yellow", (player_pos[0],player_pos[1],40,40))
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_s] and player_pos.y < 840:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a] and player_pos.x > 0:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d] and player_pos.x < 440:
-        player_pos.x += 300 * dt
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_s] and player_pos.y < 840:
+            player_pos.y += 300 * dt
+        if keys[pygame.K_a] and player_pos.x > 0:
+            player_pos.x -= 300 * dt
+        if keys[pygame.K_d] and player_pos.x < 440:
+            player_pos.x += 300 * dt
 
-    # "Y" bajando a medida que pasa el tiempo
-    if player_pos.y < 840:
-        player_pos.y += 0.5
+        # "Y" bajando a medida que pasa el tiempo
+        if player_pos.y < 840:
+            player_pos.y += 0.5
+  
+
+
 
     # flip() the display to put your work on screen
     pygame.display.flip()
